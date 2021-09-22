57628a27729b6839c9039f2c3774528ab635ba9a
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2021-09-22 11:03:46 -0700

    update dependency versions and change createTask flow

--- src/adapter/createTask.ts
+++ src/adapter/createTask.ts
@@ -7,51 +7,8 @@ import {
 import { Secrets } from 'sanity-translations-tab'
 import { getTranslationTask } from './getTranslationTask'
 
-const uploadFile = async (
-  documentId: string,
-  projectId: string,
-  document: Record<string, any>,
-  accessToken: string
-) => {
-  const url = `https://api.smartling.com/files-api/v2/projects/${projectId}/file`
-  const formData = new FormData()
-  formData.append('fileUri', documentId)
-  formData.append('fileType', 'html')
-  const htmlBuffer = Buffer.from(document.content, 'utf-8')
-  formData.append('file', new Blob([htmlBuffer]), `${document.name}.html`)
-
-  return fetch(smartlingProxy, {
-    method: 'POST',
-    headers: getHeaders(url, accessToken),
-    body: formData,
-  }).then(res => res.json())
-}
-
-const assignFileToJob = async (
-  jobId: string,
-  documentId: string,
-  projectId: string,
-  localeIds: string[],
-  accessToken: string
-) => {
-  const url = `https://api.smartling.com/jobs-api/v3/projects/${projectId}/jobs/${jobId}/file/add`
-  return fetch(smartlingProxy, {
-    method: 'POST',
-    headers: {
-      ...getHeaders(url, accessToken),
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      fileUri: documentId,
-      targetLocaleIds: localeIds,
-    }),
-  })
-    .then(res => res.json())
-    .then(res => res.response)
-}
-
 const createJob = async (
-  documentId: string,
+  jobName: string,
   projectId: string,
   localeIds: string[],
   accessToken: string
@@ -64,7 +21,7 @@ const createJob = async (
       'content-type': 'application/json',
     },
     body: JSON.stringify({
-      jobName: documentId,
+      jobName,
       targetLocaleIds: localeIds,
     }),
   })
@@ -72,13 +29,18 @@ const createJob = async (
     .then(res => res.response.data.translationJobUid)
 }
 
-const authorizeJob = async (
+/* we're using batches here because it eliminates some
+ * new string authorization issues for updating existing jobs,
+ * and is able to be used for new bulk
+ * job functionality.
+ */
+const createJobBatch = async (
   jobId: string,
   projectId: string,
-  localeIds: string[],
+  documentName: string,
   accessToken: string
 ) => {
-  const url = `https://api.smartling.com/jobs-api/v3/projects/${projectId}/jobs/${jobId}/authorize`
+  const url = `https://api.smartling.com/job-batches-api/v2/projects/${projectId}/batches`
   return fetch(smartlingProxy, {
     method: 'POST',
     headers: {
@@ -86,8 +48,36 @@ const authorizeJob = async (
       'content-type': 'application/json',
     },
     body: JSON.stringify({
-      localeWorkflows: localeIds.map(l => ({ targetLocaleId: l })),
+      authorize: true,
+      translationJobUid: jobId,
+      fileUris: [documentName],
     }),
+  })
+    .then(res => res.json())
+    .then(res => res.response.data.batchUid)
+}
+
+const uploadFileToBatch = async (
+  batchUid: string,
+  document: Record<string, any>,
+  projectId: string,
+  localeIds: string[],
+  accessToken: string
+) => {
+  const url = `https://api.smartling.com/job-batches-api/v2/projects/${projectId}/batches/${batchUid}/file`
+  const formData = new FormData()
+  formData.append('fileUri', document.name)
+  formData.append('fileType', 'html')
+  const htmlBuffer = Buffer.from(document.content, 'utf-8')
+  formData.append('file', new Blob([htmlBuffer]), `${document.name}.html`)
+  localeIds.forEach(localeId =>
+    formData.append('localeIdsToAuthorize[]', localeId)
+  )
+
+  return fetch(smartlingProxy, {
+    method: 'POST',
+    headers: getHeaders(url, accessToken),
+    body: formData,
   }).then(res => res.json())
 }
 
@@ -98,18 +88,16 @@ export const createTask = async (
   secrets: Secrets
 ) => {
   const accessToken = await authenticate(secrets.secret)
+
   //TODO: announce errors here
-  const uploadFileRes = await uploadFile(
-    documentId,
+  let taskId = await findExistingJob(
+    document.name,
     secrets.project,
-    document,
     accessToken
   )
-  console.log('uploadFileRes', uploadFileRes)
-  let taskId = await findExistingJob(documentId, secrets.project, accessToken)
   if (!taskId) {
     taskId = await createJob(
-      documentId,
+      document.name,
       secrets.project,
       localeIds,
       accessToken
@@ -117,21 +105,20 @@ export const createTask = async (
   }
 
   //TODO: log errors here if needed
-  const assignStatus = await assignFileToJob(
+  const batchUid = await createJobBatch(
     taskId,
-    documentId,
+    secrets.project,
+    document.name,
+    accessToken
+  )
+  const uploadFileRes = await uploadFileToBatch(
+    batchUid,
+    document,
     secrets.project,
     localeIds,
     accessToken
   )
-  console.log('assign status', assignStatus)
-  const authorizeStatus = await authorizeJob(
-    taskId,
-    secrets.project,
-    localeIds,
-    accessToken
-  )
-  console.log('authStatus', authorizeStatus)
+  console.log('upload status', uploadFileRes)
 
   return getTranslationTask(documentId, secrets)
 }
